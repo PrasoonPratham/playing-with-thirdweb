3734aa8f5f20f0ae0f875d1aaae8937a1da3fa57
Pratham Prasoon <prathamprasoon@Prathams-MacBook-Pro.local>
2021-11-06 14:30:57 +0530

    Changed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from nftlabs import NftlabsSdk, SdkOptions, ListArg, MintArg
 import os
 
+from nftlabs.modules.currency import CurrencyModule
+
 # Conncect to the SDK, we're using the testnet to test it
 sdk = NftlabsSdk(SdkOptions(), "https://rinkeby-light.eth.linkpool.io/")
 
@@ -10,7 +12,7 @@ sdk.set_private_key("a0064ca30a15d338590d95fc7d1e87f889d11fe31897340b391674e84d4
 
 # Initiate the modules so you can start interacting with the smart contracts. You can g=et these address in your thirdweb dashboard at https://www.thirdweb.com/dashboard
 nft_module = sdk.get_nft_module("0xAC914C57a2a263E54d6516E3a61D35dF88dF8334")
-currency_module = sdk.get_currency_module("0x10E49aaBDad7B8Aa624fbbaCD6b93DC20e3dDBcD")
+#currency_module = sdk.get_currency_module("0x10E49aaBDad7B8Aa624fbbaCD6b93DC20e3dDBcD")
 #market_module = sdk.get_market_module("0x2B1Af44BaC45Bd9C400e6388bff0DD144e6EF4e6")
 
 #print(nft_module.balance())
@@ -26,8 +28,8 @@ prop = {}
 
 amount = 1000
 
-currency_module.mint(amount* (10**18))
-print(currency_module.balance())
+# currency_module.mint(amount* (10**18))
+# print(CurrencyModule.balance())
 
 
 #nft_test.balance_of("0x55c9bBb71a5CC11c2f0c40362Bb691b33a78B764")
@@ -41,6 +43,4 @@ print(currency_module.balance())
 
 #https://rinkeby-light.eth.linkpool.io/
 #https://rinkeby.arbitrum.io/rpc
-
-
 
